bc1d16ac3edec6bd24fc06c6cb48369a68445acb
Norbert Remenyi <remenyinorbert@gmail.com>
2023-10-20 17:56:31 +0200

    feat: add routes for homepage and map

--- sarp/src/App.js
+++ sarp/src/App.js
@@ -1,7 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/homepage/HomePage'
+import NavigationBar from './components/elements/NavigationBar'
+import Map from './components/map/Map'
 
 function App () {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/map' element={<Map />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
