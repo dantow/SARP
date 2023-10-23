f9976d50f7f5c2b6c52f28070d66fed4bb21ce01
Norbert Remenyi <remenyinorbert@gmail.com>
2023-10-23 12:56:13 +0200

    Chore: Refactor Haversine Formula and move Degree to separate method Conversion .

--- sarp/src/__test__/haversineDistance.test.js
+++ sarp/src/__test__/haversineDistance.test.js
@@ -2,12 +2,8 @@
 import haversineDistance from '../components/utils/haversine'
 
 test('Calculated haversine distance is in 20 meter from base coordinate', () => {
-  const baseLatitude = 47.102809
-  const baseLongitude = 17.909302
-  const twentyMeterFromBaseLatitude = 47.102934
-  const twentyMeterFromBaseLongitude = 17.909492
-  const baseCoordinates = { lat: baseLatitude, lng: baseLongitude }
-  const twentyMeterFromBaseCoordinates = { lat: twentyMeterFromBaseLatitude, lng: twentyMeterFromBaseLongitude }
+  const baseCoordinates = { lat: 47.102809, lng: 17.909302 }
+  const twentyMeterFromBaseCoordinates = { lat: 47.102934, lng: 17.909492 }
 
   const result = haversineDistance(baseCoordinates, twentyMeterFromBaseCoordinates)
 
